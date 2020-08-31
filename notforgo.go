1b7b5144a28f634e8070d19ee7adf950dbe70337
Olaf Olszak <ulubiony.smiechotek@gmail.com>
2020-08-31 16:45:28 +0200

    Add notforgo.go

--- notforgo.go
+++ notforgo.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("notes repository by Nietworzy is not for Go.")
+	fmt.Println("go command is not for download notes repository by Nietworzy!")
 }
